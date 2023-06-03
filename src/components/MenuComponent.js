68e53702af03deba7a7a797a200a5ac70ece3aef
justbebetter <davidtranit110@gmail.com>
2023-06-03 12:37:06 +0700

    Presentational and Container Components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Media } from 'reactstrap';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
   CardTitle } from 'reactstrap';
+import DishDetail from './DishdetailComponent';  
 
 class Menu extends Component {
     constructor(props) {
@@ -10,31 +11,13 @@ class Menu extends Component {
           selectedDish: null
       }
     }
-    onDishSelect(dish) {
-      this.setState({ selectedDish: dish});
-    }
-    renderDish(dish) {
-      if (dish != null)
-          return(
-              <Card>
-                  <CardImg top src={dish.image} alt={dish.name} />
-                  <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                  </CardBody>
-              </Card>
-          );
-      else
-          return(
-              <div></div>
-          );
-  }
+  
 render() {
       const menu = this.props.dishes.map((dish) => {
           return (
             <div  className="col-12 col-md-5 m-1">
               <Card key={dish.id}
-                onClick={() => this.onDishSelect(dish)}>
+                onClick={() => this.props.onClick(dish.id)}>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
@@ -49,11 +32,7 @@ render() {
               <div className="row">
                   {menu}
               </div>
-              <div className="row">
-                <div  className="col-12 col-md-5 m-1">
-                  {this.renderDish(this.state.selectedDish)}
-                </div>
-              </div>
+              <DishDetail selecteddish={this.state.selectedDish} />
           </div>
       );
   }
--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -14,35 +14,35 @@ export const DISHES =
             rating: 5,
             comment: "Imagine all the eatables, living in conFusion!",
             author: "John Lemon",
-            date: "2012-10-16T17:57:28.556094Z"
+            date: "2023-10-16T17:57:28.556094Z"
             },
             {
             id: 1,
             rating: 4,
             comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-            author: "Paul McVites",
-            date: "2014-09-05T17:57:28.556094Z"
+            author: "David Tran",
+            date: "2023-09-05T17:57:28.556094Z"
             },
             {
             id: 2,
             rating: 3,
             comment: "Eat it, just eat it!",
             author: "Michael Jaikishan",
-            date: "2015-02-13T17:57:28.556094Z"
+            date: "2023-02-13T17:57:28.556094Z"
             },
             {
             id: 3,
             rating: 4,
             comment: "Ultimate, Reaching for the stars!",
             author: "Ringo Starry",
-            date: "2013-12-02T17:57:28.556094Z"
+            date: "2023-12-02T17:57:28.556094Z"
             },
             {
             id: 4,
             rating: 2,
             comment: "It's your birthday, we're gonna party!",
             author: "25 Cent",
-            date: "2011-12-02T17:57:28.556094Z"
+            date: "2023-12-02T17:57:28.556094Z"
             }
         ]                        },
         {
@@ -59,35 +59,35 @@ export const DISHES =
             rating: 5,
             comment: "Imagine all the eatables, living in conFusion!",
             author: "John Lemon",
-            date: "2012-10-16T17:57:28.556094Z"
+            date: "2023-10-16T17:57:28.556094Z"
             },
             {
             id: 1,
             rating: 4,
             comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
             author: "Paul McVites",
-            date: "2014-09-05T17:57:28.556094Z"
+            date: "2023-09-05T17:57:28.556094Z"
             },
             {
             id: 2,
             rating: 3,
             comment: "Eat it, just eat it!",
             author: "Michael Jaikishan",
-            date: "2015-02-13T17:57:28.556094Z"
+            date: "2023-02-13T17:57:28.556094Z"
             },
             {
             id: 3,
             rating: 4,
             comment: "Ultimate, Reaching for the stars!",
             author: "Ringo Starry",
-            date: "2013-12-02T17:57:28.556094Z"
+            date: "2023-12-02T17:57:28.556094Z"
             },
             {
             id: 4,
             rating: 2,
             comment: "It's your birthday, we're gonna party!",
             author: "25 Cent",
-            date: "2011-12-02T17:57:28.556094Z"
+            date: "2023-12-02T17:57:28.556094Z"
             }
         ]
         },
@@ -105,35 +105,35 @@ export const DISHES =
             rating: 5,
             comment: "Imagine all the eatables, living in conFusion!",
             author: "John Lemon",
-            date: "2012-10-16T17:57:28.556094Z"
+            date: "2023-10-16T17:57:28.556094Z"
             },
             {
             id: 1,
             rating: 4,
             comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
             author: "Paul McVites",
-            date: "2014-09-05T17:57:28.556094Z"
+            date: "2023-09-05T17:57:28.556094Z"
             },
             {
             id: 2,
             rating: 3,
             comment: "Eat it, just eat it!",
             author: "Michael Jaikishan",
-            date: "2015-02-13T17:57:28.556094Z"
+            date: "2023-02-13T17:57:28.556094Z"
             },
             {
             id: 3,
             rating: 4,
             comment: "Ultimate, Reaching for the stars!",
             author: "Ringo Starry",
-            date: "2013-12-02T17:57:28.556094Z"
+            date: "2023-12-02T17:57:28.556094Z"
             },
             {
             id: 4,
             rating: 2,
             comment: "It's your birthday, we're gonna party!",
             author: "25 Cent",
-            date: "2011-12-02T17:57:28.556094Z"
+            date: "2023-12-02T17:57:28.556094Z"
             }
         ]
         },
